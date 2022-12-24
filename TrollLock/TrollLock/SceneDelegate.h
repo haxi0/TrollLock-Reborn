fa6e87deda2b93231fe9bcb70d8185d5284d7ad3
haxi0 <85764897+haxi0@users.noreply.github.com>
2022-12-24 13:59:45 +0300

    Add files via upload

---
+++ TrollLock/TrollLock/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  POCTester
+//
+//  Created by Анохин Юрий on 20.12.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TrollLock/TrollLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  POCTester
+//
+//  Created by Анохин Юрий on 20.12.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIActivityIndicatorView *Active;
+
+@end
+
