29e95012051fc9f78e4865f32a3202046bcc04d9
aethercot <99132199+aethercot@users.noreply.github.com>
2022-12-26 23:54:17 +0100

    feat:trollPrepare functions
    Does what every option in ViewController.m does excluding running the exploit.
    
    This allows for cleaner and more readable code. :smiley:

---
+++ TrollLock/TrollLock/TrollPrepare.h
@@ -0,0 +1,13 @@
+//
+//  TrollPrepare.h
+//  TrollLock
+//
+//  Created by Apricot on 2022-12-26.
+//
+
+#ifndef TrollPrepare_h
+#define TrollPrepare_h
+
+void trollPrepare(void);
+
+#endif /* TrollPrepare_h */
